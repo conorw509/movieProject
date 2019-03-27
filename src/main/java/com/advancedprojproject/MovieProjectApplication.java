5e8fe1fb3d562e6eed901d514a03ce5e54cf9d55
Conor Ward <conorward509@hotmail.com>
2019-03-27 13:19:02 +0000

    first commit

---
+++ src/main/java/com/advancedprojproject/MovieProjectApplication.java
@@ -0,0 +1,14 @@
+package com.advancedprojproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieProjectApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(MovieProjectApplication.class, args);
+    }
+
+}
+//hello
