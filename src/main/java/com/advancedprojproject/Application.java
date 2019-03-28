b78645f8827fba773b717a6b5d03f90be0d13045
Conor Ward <conorward509@hotmail.com>
2019-03-28 01:17:21 +0000

    working

--- src/main/java/com/advancedprojproject/Application.java
+++ src/main/java/com/advancedprojproject/Application.java
@@ -1,13 +1,14 @@
 package com.advancedprojproject;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MovieProjectApplication {
+public class Application {
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(MovieProjectApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
     }
 
 }
