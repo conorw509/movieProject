ec70285a17a2ec60b874efbfda20a0b226fe2789
Conor Ward <conorward509@hotmail.com>
2019-03-28 12:38:37 +0000

    working

--- src/main/java/controller/WebController.java
+++ src/main/java/controller/WebController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
